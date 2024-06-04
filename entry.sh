2e066caee8645fcdbd0ece8d61dd990d5f9dbf0b
arash16 <arash.shakery@gmail.com>
2024-06-04 16:38:02 +0330

    cleaner entry.sh

--- entry.sh
+++ entry.sh
@@ -3,11 +3,8 @@
 set -xe
 
 # =============================== wireguard ===================================
-if [ `grep -c "162.159.192.1 engage.cloudflareclient.com" /etc/hosts` == '0' ]; then
-  echo "162.159.192.1 engage.cloudflareclient.com" >> /etc/hosts
-fi
-
-[[ "$CF" == "true" ]] && ./wgcf-docker.sh
+echo "162.159.192.1 engage.cloudflareclient.com" >> /etc/hosts
+./wgcf-docker.sh
 
 # ================================= server ====================================
 node index
